91266efbf5bfd3fa8491f52bce240a789afc467d
Zach Brown <bigcountry66789@gmail.com>
2026-01-07 20:43:04 -0600

    Wrote new code to tell who drinks what

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CodeWars_Bartender;
+
+public class Kata
+{
+    public static string GetDrinkByProfession(string p)
+    {
+        string profession = "";
+        switch(p.ToLower()){
+            case "jabroni":
+                return ("Patron Tequila");
+                break;
+            case "school counselor":
+                return ("Anything with Alcohol");
+                break;
+            case "programmer":
+                return ("Hipster Craft Beer");
+                break;
+            case "bike gang member":
+                return ("Moonshine");
+                break;
+            case "politician":
+                return ("Your tax dollars");
+                break;
+            case "rapper":
+                return ("Cristal");
+                break;
+            default:
+                return ("Beer");
+                break;
+        }
+    }
+}
